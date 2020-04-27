d1c51992831fcece7724cedc71e8c7c175fc1e6f
humblego <injun.woo30000@gmail.com>
2020-04-28 00:13:21 +0900

    Modify: changed too long variable names

--- scheduled_actions.py
+++ scheduled_actions.py
@@ -7,24 +7,24 @@ sched = BlockingScheduler()
 
 def make_match():
     unmatched_users = User.query.filter_by(joined=True).all()
-    matched_users_groups = []
+    matched_groups = []
     matches = []
     count_unmatched_users = len(unmatched_users)
     while count_unmatched_users >= 2:
-        matched_users_group = sample(unmatched_users, 2)
-        matched_users_groups += [matched_users_group]
+        matched_group = sample(unmatched_users, 2)
+        matched_groups += [matched_group]
         for i in range(2):
-            unmatched_users.remove(matched_users_group[i])
+            unmatched_users.remove(matched_group[i])
         count_unmatched_users -= 2
     if count_unmatched_users == 1:
         unmatched_users[0].joined = False
         # NEED MATCH FAIL HANDLING
-    for matched_users_group in matched_users_groups:
-        matched_users_group[0].joined = False
-        matched_users_group[1].joined = False
+    for matched_group in matched_groups:
+        matched_group[0].joined = False
+        matched_group[1].joined = False
         match = Match(
-            user1=matched_users_group[0],
-            user2=matched_users_group[1]
+            user1=matched_group[0],
+            user2=matched_group[1]
         )
         matches.append(match)
     db.session.add_all(matches)
