b3b8c915212655c049ff704f2702fdcd19924234
eunhkim <valhalla.host@gmail.com>
2020-04-29 18:02:26 +0900

    ADD: eph message in command_view function

--- app.py
+++ app.py
@@ -189,21 +189,25 @@ def get_base_blocks(text)
     ]
     return blocks
 
+
 @app.route("/slack/command", methods=['POST'])
-def command_view():
+def command_main():
     slack_id = request.form.getlist('user_id')
     user_name = request.form.getlist('user_name')
+    call_channel = request.form.getlist('channel_id')
     command_time = datetime.utcnow()
     if command_time.hour == 8 and command_time.minute > 20: #14, 42
-        blocks = get_base_blocks("지금은 매칭을 준비중입니다.")
+        eph_blocks = get_base_blocks("지금은 매칭을 준비중입니다.")
     else:
+        eph_blocks = get_base_blocks("디엠을 확인해주세요!")
         user_state = get_user_state(slack_id)
         blocks = get_blocks(user_state)
         response = slack.conversations.open(users=slack_id, return_im=True)
-        channel = response.body['channel']['id']
+        dm_channel = response.body['channel']['id']
         if user_state is None:  # 처음 등록했을 경우
             create_user(slack_id[0], user_name[0])
-        slack.chat.post_message(channel=channel, blocks=json.dumps(blocks))
+        slack.chat.post_message(channel=dm_channel, blocks=blocks)
+    slack.chat.post_ephemeral(channel=call_channel, user=slack_id, blocks=json.dumps(blocks))
     return ("", 200)
 
 
