775318c46f126b965feadacb2e9dea080fe85b78
eunhkim <valhalla.host@gmail.com>
2020-05-02 03:16:45 +0900

    EDIT: init random activity in create match code

--- scheduled_actions.py
+++ scheduled_actions.py
@@ -10,27 +10,25 @@ sched = BlockingScheduler()
 
 
 def match_failed_handling(unmatched_user):
+    print("MATCH FAILED HANDLING")
     slack_id = unmatched_user.slack_id
+    print("_SLACK_ID: " + str(slack_id))
     intra_id = unmatched_user.intra_id
     response = slack.conversations.open(users=slack_id, return_im=True)
     channel = response.body['channel']['id']
     blocks = get_base_blocks("MATCH FAILED. SORRY, " + str(intra_id) + "!")
     slack.chat.post_message(channel=channel, blocks=json.dumps(blocks))
-    print("MATCH FAILED HANDLING")
-    print("_SLACK_ID: " + str(slack_id))
     return ("", 200)
 
 
 def match_successed_handling(matches):
     print("MATCH SUCCESSED HANDLING")
-    activities = Activity.query.all()
     for match in matches:
         slack_id = [match.users[0].slack_id, match.users[1].slack_id]
         print("_SLACK_ID: " + str(slack_id[0]) + " & " + str(slack_id[1]))
         response = slack.conversations.open(users=slack_id, return_im=True)
         channel = response.body['channel']['id']
-        activity = sample(activities, 1)[0]
-        blocks = get_match_blocks(match.users, activity)
+        blocks = get_match_blocks(match)
         slack.chat.post_message(channel=channel, blocks=json.dumps(blocks))
     return ("", 200)
 
@@ -68,12 +66,15 @@ def make_match():
     if count_unmatched_users == 1:
         unmatched_users[0].joined = False
         match_failed_handling(unmatched_users[0])
+    activities = Activity.query.all()
     for matched_group in matched_groups:
         matched_group[0].joined = False
         matched_group[1].joined = False
+        activity = sample(activities, 1)[0]
         match = Match(
             user1=matched_group[0],
-            user2=matched_group[1]
+            user2=matched_group[1],
+            activity=activity
         )
         matches.append(match)
     match_successed_handling(matches)
@@ -83,5 +84,6 @@ def make_match():
 
 
 sched.add_job(make_match, 'cron', hour=15, minute=00)
-
 sched.start()
+
+make_match()
